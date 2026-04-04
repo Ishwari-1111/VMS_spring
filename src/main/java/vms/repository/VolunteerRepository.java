540f73d399d605637e1d5377f869ed10bd9ff13c
ReemaDesle <desle.reema@gmail.com>
2026-04-04 22:02:07 +0530

    feat: volunteer module - entity, repository, service

--- src/main/java/vms/repository/VolunteerRepository.java
+++ src/main/java/vms/repository/VolunteerRepository.java
@@ -1,5 +1,18 @@
 package vms.repository;
 
-public class VolunteerRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import vms.model.Volunteer;
+
+@Repository
+public interface VolunteerRepository
+        extends JpaRepository<Volunteer, String> {
+
+    // JpaRepository already gives you:
+    // save(volunteer)        replaces addVolunteer()
+    // deleteById(id)         replaces removeVolunteer()
+    // findById(id)           replaces getVolunteerById()
+    // findAll()              replaces getAllVolunteers()
+    // existsById(id)         replaces containsVolunteer()
+    // count()                replaces getVolunteerCount()
 }
